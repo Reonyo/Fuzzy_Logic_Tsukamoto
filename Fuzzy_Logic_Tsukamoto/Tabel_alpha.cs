e690d0d93e0cf48370e08a8be2f29b34b998c77a
Reonyo <151089826+Reonyo@users.noreply.github.com>
2023-12-16 14:30:41 +0700

    Update Tabel_alpha.cs

--- Fuzzy_Logic_Tsukamoto/Tabel_alpha.cs
+++ Fuzzy_Logic_Tsukamoto/Tabel_alpha.cs
@@ -9,15 +9,15 @@ namespace Fuzzy_Logic_Tsukamoto
     public class AlphaData
     {
         public string NilaiAlpha { get; set; }
-        public double PersediaanBanyak { get; set; }
-        public double PersediaanSedang { get; set; }
-        public double PersediaanSedikit { get; set; }
+        public double PenjualanBanyak { get; set; }
+        public double PenjualanSedang { get; set; }
+        public double PenjualanSedikit { get; set; }
     }
     public class ZData
     {
         public string NilaiZ { get; set; }
-        public double PersediaanBanyak { get; set; }
-        public double PersediaanSedang { get; set; }
-        public double PersediaanSedikit { get; set; }
+        public double PenjualanBanyak { get; set; }
+        public double PenjualanSedang { get; set; }
+        public double PenjualanSedikit { get; set; }
     }
 }
